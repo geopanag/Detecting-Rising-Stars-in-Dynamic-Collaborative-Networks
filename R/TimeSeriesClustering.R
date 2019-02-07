afa55ee5653fab3c92af297a3113506984eb2b8e
Gpan <georgepanagopoulos5@yahoo.gr>
2019-02-07 12:38:38 +0100

    -

--- R/TimeSeriesClustering.R
+++ R/TimeSeriesClustering.R
@@ -1,12 +1,12 @@
 setwd("/data/finals")
 
+library(fpc)  
+library(dtw)   
+
 
 ts=read.csv("timeSeries.csv",header=FALSE)
 labTs=ts[,1]
 ts=ts[,-1]
-
-library(fpc)  
-library(dtw)   
 
 p=pam(ts,7,diss=FALSE,metric='DTW')
 write.csv(cbind(labTs,p$clustering),"tsPam.csv")
